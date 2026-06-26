abf5cfbefb93e1cfaafa69f4c17e04401c7dc7fd
juliMarquesSouza <ju.marquess2023@gmail.com>
2026-06-26 16:42:42 -0300

    fix prisma sqlite adapter

--- src/server.js
+++ src/server.js
@@ -7,9 +7,9 @@ const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 
 const { PrismaClient } = require('@prisma/client');
-const { PrismaBetterSQLite3 } = require('@prisma/adapter-better-sqlite3');
+const { PrismaBetterSqlite3 } = require('@prisma/adapter-better-sqlite3');
 
-const adapter = new PrismaBetterSQLite3({
+const adapter = new PrismaBetterSqlite3({
   url: process.env.DATABASE_URL || 'file:./dev.db',
 });
 
